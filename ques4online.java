b0a14089dffc5f3be3686205728ac32267deba4a
Harsh Gupta <67871076+harsh0512@users.noreply.github.com>
2020-12-02 20:07:25 +0530

    WAP to find whether a number is Prime or not

---
+++ ques4online.java
@@ -0,0 +1,26 @@
+package Assignement;
+
+import java.util.Scanner;
+public class ques4online{    
+    public static void main(String args[]){    
+     int i,m=0,flag=0;      
+     int n;
+     Scanner obj = new Scanner(System.in);
+        System.out.println("Enter the Number");
+        i=obj.nextInt();
+        n=i;    
+     m=n/2;      
+     if(n==0||n==1){  
+      System.out.println(n+" is not prime number");      
+     }else{  
+      for(i=2;i<=m;i++){      
+       if(n%i==0){      
+        System.out.println(n+" is not prime number");      
+        flag=1;      
+        break;      
+       }      
+      }      
+      if(flag==0)  { System.out.println(n+" is prime number"); }  
+     } 
+   }    
+   } 
