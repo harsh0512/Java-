a827f87411388108f4cf2f2793e8222f2b065a87
Harsh Gupta <67871076+harsh0512@users.noreply.github.com>
2020-12-02 21:56:56 +0530

    WAP to print "Hello World" input from Keyboard

---
+++ ques1.java
@@ -0,0 +1,10 @@
+package Assignement;
+import java.util.Scanner; 
+public class ques1 {
+    public static void main(String[] args){
+    String greet;
+    Scanner sc= new Scanner(System.in);
+    greet=sc.nextLine();
+    System.out.println(greet);
+}
+}
